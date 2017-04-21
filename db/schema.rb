69ad2565444f60103376d90a65c986616137f796
Marcela Pino <marcelap99@aol.com>
2017-04-21 11:50:04 -0400

    commit

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 20170419214602) do
+ActiveRecord::Schema.define(version: 20170419214603) do
 
   create_table "posts", force: :cascade do |t|
     t.string  "title"
