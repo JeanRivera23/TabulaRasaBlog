3dacff4270bd172bb10cf1ee10793738db73e14d
Jeanette Rivera <jeanetterivera465@gmail.com>
2017-04-21 19:53:03 -0400

    all database homework complete

--- app.rb
+++ app.rb
@@ -17,12 +17,19 @@ end
 post "/" do
   @user = User.where(fname: params[:user][:fname]).first
 
-  if @user.password == params[:user][:password]
+  if @user == nil
+
+    flash[:notice] = "Your name or password do not exist."
+
+    redirect "/"
+
+  elsif @user.password == params[:user][:password]
     session[:user_id] = @user.id
 
     flash[:notice] = "Welcome!"
 
     redirect "/feed"
+
   else
     flash[:notice] = "Your name and password do not match."
 
@@ -53,6 +60,7 @@ get "/feed" do
   erb :feed
 end
 
+
 post "/feed" do
   @post = Post.create(
     title: params[:post][:title], content: params[:post][:content], user_id: session[:user_id]
@@ -60,25 +68,45 @@ post "/feed" do
   redirect "/feed"
 end
 
+
 get "/profile" do
   @user = User.find(session[:user_id])
-  # @posts = Post.find_by(user_id: session[:user_id])
   erb :profile
 end
+
+post "/profile" do
+  @user = User.find(session[:user_id])
+
+  @user.update(fname: params[:user][:fname],
+  lname: params[:user][:lname],
+  password: params[:user][:password])
+
+  erb :profile
+end
+
+
+get '/visit_profile/:id' do
+  @user = User.find(params[:id])
+  erb :visit_profile
+end
+
 
 get '/sign-out' do
   session[:user_id] = nil
   redirect "/"
 end
 
+
 get '/account_delete' do
   @user = User.find(session[:user_id])
 
   @user.destroy
 
-  erb :account_delete
+  erb :sign_in
 end
 
+
+# don't think this is necessary, can delete
 def current_user
   if session[:user_id]
    User.find(session[:user_id])
--- public/main.js
+++ public/main.js
@@ -1,4 +1,11 @@
 document.addEventListener("DOMContentLoaded", function (event) {
+  var edit = document.getElementById("edit")
+  var editForm = document.getElementById("editForm")
 
+  editForm.style.display = "none"
+
+  edit.addEventListener("click", function() {
+    editForm.style.display = "block"
+  })
 
 })
